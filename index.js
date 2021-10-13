d40e5296171bdf4a065dd7625ac4328a138cec35
amitkapoor403 <85727075+amitkapoor403@users.noreply.github.com>
2021-10-13 10:18:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Button press
+//Button press
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
@@ -7,61 +7,60 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
         var buttonInnerHTML = this.innerHTML;
 
-        makeSound(buttonInnerHTML);
 
-        buttonAnimation(buttonInnerHTML);
+        makeSound(buttonInnerHTML);
+            buttonAnimation(buttonInnerHTML);
+
 
     });
 
 }
 
 
-// Keyboard press
+//Keyboard press
 document.addEventListener("keypress", function(event) {
 
     makeSound(event.key);
-    buttonAnimation(event.key);
+     buttonAnimation(event.key);
 
-});
-
-
+})
 
 function makeSound(key) {
     switch (key) {
         case "w":
-            var audio = new Audio('sounds/tom-1.mp3');
+            var audio = new Audio('tom-1.mp3');
             audio.play();
             break;
 
         case "a":
-            var audio = new Audio('sounds/tom-2.mp3');
+            var audio = new Audio('tom-2.mp3');
             audio.play();
             break;
 
 
         case "s":
-            var audio = new Audio('sounds/tom-3.mp3');
+            var audio = new Audio('tom-3.mp3');
             audio.play();
             break;
 
 
         case "d":
-            var audio = new Audio('sounds/tom-4.mp3');
+            var audio = new Audio('tom-4.mp3');
             audio.play();
             break;
 
         case "j":
-            var audio = new Audio('sounds/snare.mp3');
+            var audio = new Audio('snare.mp3');
             audio.play();
             break;
 
         case "k":
-            var audio = new Audio('sounds/crash.mp3');
+            var audio = new Audio('crash.mp3');
             audio.play();
             break;
 
         case "l":
-            var audio = new Audio('sounds/kick-bass.mp3');
+            var audio = new Audio('kick-bass.mp3');
             audio.play();
             break;
 
@@ -69,8 +68,6 @@ function makeSound(key) {
     }
 
 }
-
-
 
 function buttonAnimation(currentKey) {
 
